369184349e383fa992b7bc00ce0bbc25eb34472f
Fedrik Pangestu <fedrikpangestu1@gmail.com>
2024-08-28 16:35:06 +0700

    fedrik

---
+++ lib/pertemuan3.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePertemuan3 extends StatefulWidget {
+  const HomePertemuan3({super.key});
+
+  @override
+  State<HomePertemuan3> createState() => _HomePertemuan3State();
+}
+
+class _HomePertemuan3State extends State<HomePertemuan3> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
